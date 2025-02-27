6722c40bcf06523735228ef01ecb99a01c5047d1
ivtomov <92026742+ivtomov@users.noreply.github.com>
2025-02-27 12:30:45 +0200

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -3,37 +3,37 @@
 #include <stdlib.h>
 
 //Задача 1. Предаване на константен псевдоним на обект като параметър на функция.
-class point //Клас точка
+class point 
 { private:
 double x;
 double y;
 public:
-point(double xcoord, double ycoord); //Конструктор с два параметъра
-void print(); //Печат на точка
-double dist(const point &p1, const point &p2); //Разстояние между две точки
-~point(); //Деструктор
+point(double xcoord, double ycoord); 
+void print(); 
+double dist(const point &p1, const point &p2); 
+~point(); 
 };
-point::point(double xcoord, double ycoord) //Конструктор с два параметъра
+point::point(double xcoord, double ycoord) 
 {
 cout << ”Creating Point(” << xcoord << ”,” << ycoord << ”)” << endl;
 x = xcoord;
 y = ycoord;
 }
-void point::print() //Печат на точка
+void point::print()
 {
 cout << ”Point(” << x << ”,” << y << ”)”;
 }
-double point::dist(const point &p1, const point &p2) // Разстояние между две точки
+double point::dist(const point &p1, const point &p2) 
 {
 return sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
 }
-point::~point() //Деструктор
+point::~point() 
 { cout << ”Destructing ”;
 print();
 cout << endl;
 }
 void main()
-{ point a(3,4), b(10,4); //Обекти от тип Точка
+{ point a(3,4), b(10,4);
 cout << ”Distance between ”; a.print();
 cout << ” and ”; b.print();
 cout << ” is ” << a.dist(a,b) << endl;
@@ -59,21 +59,21 @@ exit(1);
 }
 size = num;
 }
-char &array::put(int i) //Поставя се нещо в масива
+char &array::put(int i) 
 {
 if(i<0 || i>=size) {
 cout << ”Bounds error!!!\n”;
 exit(1);
 }
-return p[i]; //връща псевдоним за p[i]
+return p[i];
 }
-char array::get(int i) //Взема се нещо от масива.
+char array::get(int i) 
 {
 if(i<0 || i>=size) {
 cout << ”Bounds error!!!\n”;
 exit(1);
 }
-return p[i]; //връща символ
+return p[i]; 
 }
 int main()
 {
@@ -82,7 +82,7 @@ a.put(3) = ’X’;
 a.put(2) = ’R’;
 cout << a.get(3) << ”\n” << a.get(2);
 cout << ”\n”;
-a.put(11) = ’!’; //сега се генерира грешка по време на изпълнение
+a.put(11) = ’!’;
 return 0;
 }
 
